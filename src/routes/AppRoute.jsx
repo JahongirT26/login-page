33d46113375388a3ef0656e850d76b457a11a2c9
Tolaganov Jahongir <jahongirtolaganov26@gmail.com>
2026-06-26 05:15:51 +0500

    feat(auth): prepare register page

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import AuthLayout from "../layouts/AuthLayout"
 import LoginPage from "../pages/LoginPage"
+import RegisterPage from "../pages/RegisterPage"
 
 function AppRoutes() {
     return (
         <Routes>
             <Route element={<AuthLayout />}>
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
             </Route>
         </Routes>
     )
